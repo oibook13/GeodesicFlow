034069adac1f0e437b0b6ea7d2b575392e78b90b
Drake Du <drakezdu@gmail.com>
2025-09-20 19:52:28 -0400

    Drake adjusted resources.

--- train_drake_1.sh
+++ train_drake_1.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name=train_drake_1
 #SBATCH --account=kempner_undergrads
-#SBATCH --partition=kempner_h100
+#SBATCH --partition=kempner
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=24
-#SBATCH --gpus-per-node=2
-#SBATCH --time=2-00:00
-#SBATCH --mem=180G
-#SBATCH --output=%x.out
-#SBATCH --error=%x.err
+#SBATCH --cpus-per-task=16
+#SBATCH --gpus-per-node=4
+#SBATCH --time=0-06:00
+#SBATCH --mem=60G
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --error=logs/%x_%j.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=drakedu@college.harvard.edu
 
-CUDA_VISIBLE_DEVICES=0,1 CONFIG_ENV_FILE='config/config_proposed.env' CONFIG_JSON_FILE='config/config_coco17_sd35_proposed.json' CONFIG_BACKEND=json DISABLE_UPDATES=1 srun ./train_proposed.sh
+CUDA_VISIBLE_DEVICES=0,1,2,3 CONFIG_ENV_FILE='config/config_proposed.env' CONFIG_JSON_FILE='config/config_coco17_sd35_proposed.json' CONFIG_BACKEND=json DISABLE_UPDATES=1 ./train_proposed.sh
