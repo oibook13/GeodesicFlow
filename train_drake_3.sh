9f7a17dd4b5bc221d963b73f4f63629bb644d21f
Drake Du <drakezdu@gmail.com>
2025-09-21 17:46:31 -0400

    Drake adjusted files for new task.

---
+++ train_drake_3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=train_drake_3
+#SBATCH --account=kempner_undergrads
+#SBATCH --partition=kempner_h100
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=96
+#SBATCH --gpus-per-node=4
+#SBATCH --time=0-24:00
+#SBATCH --mem=80G
+#SBATCH --output=/n/home05/drakedu/GeodesicFlow/logs/%x_%j.out
+#SBATCH --error=/n/home05/drakedu/GeodesicFlow/logs/%x_%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=drakedu@college.harvard.edu
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 CONFIG_ENV_FILE='config/config_base.env' CONFIG_JSON_FILE='config/config_coco17_flux.json' CONFIG_BACKEND=json DISABLE_UPDATES=1 ./train_proposed.sh
